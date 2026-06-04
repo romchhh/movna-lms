c2c4bf282d6ab1a944ba66b4c5ecaa132a1d1718
nowayrm <your.email@example.com>
2026-06-04 17:40:35 +0300

    some updates

--- backend/run.sh
+++ backend/run.sh
@@ -1,10 +1,5 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
+#!/bin/bash
 cd "$(dirname "$0")"
-
-
 source myenv/bin/activate
-
-
-exec uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
+nohup uvicorn app.main:app --reload --host 0.0.0.0 --port 8000 > /dev/null 2>&1 &
+echo "Backend started"
