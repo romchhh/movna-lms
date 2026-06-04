588e342f443d077564ce58f41d5c47663222756c
nowayrm <your.email@example.com>
2026-06-04 17:38:38 +0300

    some updates

--- backend/run.sh
+++ backend/run.sh
@@ -3,13 +3,8 @@ set -euo pipefail
 
 cd "$(dirname "$0")"
 
-if [[ ! -d venv ]]; then
-  echo "Створюю venv..."
-  python3 -m venv venv
-  source venv/bin/activate
-  pip install -r requirements.txt
-else
-  source venv/bin/activate
-fi
+
+source myenv/bin/activate
+
 
 exec uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
